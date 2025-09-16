82e74e29251b7848d2e72aaba7f55a775ff531de
Dawid Magnucki <david.magnucki@gmail.com>
2025-09-16 18:34:25 +0200

    Writing tests

--- src/test/java/com/kodilla/GameLogicSuiteTests.java
+++ src/test/java/com/kodilla/GameLogicSuiteTests.java
@@ -7,61 +7,81 @@ public class GameLogicSuiteTests {
 
     @Test
     void testOWinsInRow() {
+        //Given
         GameLogic game = new GameLogic();
+        //When
         game.makeMove(0,0,'O');
         game.makeMove(0,1,'O');
         game.makeMove(0,2,'O');
+        //Then
         assertTrue(game.checkWin('O'));
     }
 
     @Test
     void testOWinsInColumn() {
+        //Given
         GameLogic game = new GameLogic();
+        //When
         game.makeMove(0,1,'O');
         game.makeMove(1,1,'O');
         game.makeMove(2,1,'O');
+        //Then
         assertTrue(game.checkWin('O'));
     }
 
     @Test
     void testOWinsInDiagonal() {
+        //Given
         GameLogic game = new GameLogic();
+        //When
         game.makeMove(0,0,'O');
         game.makeMove(1,1,'O');
         game.makeMove(2,2,'O');
+        //Then
         assertTrue(game.checkWin('O'));
     }
 
     @Test
     void testXWinsInRow() {
+        //Given
         GameLogic game = new GameLogic();
+        //When
         game.makeMove(2,0,'X');
         game.makeMove(2,1,'X');
         game.makeMove(2,2,'X');
+        //Then
         assertTrue(game.checkWin('X'));
     }
 
     @Test
     void testXWinsInColumn() {
+        //Given
         GameLogic game = new GameLogic();
+        //When
         game.makeMove(0,2,'X');
         game.makeMove(1,2,'X');
         game.makeMove(2,2,'X');
+        //Then
         assertTrue(game.checkWin('X'));
     }
 
     @Test
     void testXWinsInDiagonal() {
+        //Given
         GameLogic game = new GameLogic();
+        //When
         game.makeMove(0,2,'X');
         game.makeMove(1,1,'X');
         game.makeMove(2,0,'X');
+        //Then
         assertTrue(game.checkWin('X'));
     }
 
     @Test
     void testDraw() {
+        //Given
         GameLogic game = new GameLogic();
+        //When
         game.makeMove(0,0,'X');
         game.makeMove(0,1,'O');
         game.makeMove(0,2,'X');
@@ -71,6 +91,7 @@ public class GameLogicSuiteTests {
         game.makeMove(2,0,'O');
         game.makeMove(2,1,'X');
         game.makeMove(2,2,'X');
+        //Then
         assertTrue(game.isBoardFull());
         assertFalse(game.checkWin('X'));
         assertFalse(game.checkWin('O'));
@@ -78,8 +99,11 @@ public class GameLogicSuiteTests {
 
     @Test
     void testInvalidMoveThrowsException() {
+        //Given
         GameLogic game = new GameLogic();
+        //When
         game.makeMove(1,1,'X');
+        //Then
         assertThrows(IllegalArgumentException.class, () -> game.makeMove(1,1,'O'));
     }
 }
